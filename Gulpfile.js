31b9ea08ada37434cdc0e9ec628659e3c85da451
charles peters <charlespeters42@gmail.com>
2015-02-08 21:43:18 -0500

    making adjustments
    typography
    state
    adding gulp
    and gitignore

---
+++ Gulpfile.js
@@ -0,0 +1,21 @@
+var gulp     		 = require('gulp'),
+		sass         = require('gulp-sass'),
+		plumber      = require('gulp-plumber'),
+		autoprefixer = require('gulp-autoprefixer'),
+		pixrem       = require('gulp-pixrem');
+
+gulp.task('styles', function(){
+	gulp.src(['_embark.scss','./base/*.scss'])
+	.pipe(plumber())
+	.pipe(sass({outputStyle: 'compressed'}))
+	.pipe(autoprefixer('last 2 version'))
+	.pipe(pixrem())
+	.pipe(gulp.dest('./css/'));
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch(['_embark.scss','./base/*.scss'], ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
