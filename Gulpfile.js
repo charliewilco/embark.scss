a98a67ee73ee4b17c3f0e3e477445c23b001fe45
charles peters <charlespeters42@gmail.com>
2015-03-15 01:50:16 -0400

    Breaking apart the base scss files

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,14 +2,16 @@ var gulp     		 = require('gulp'),
 		sass         = require('gulp-sass'),
 		plumber      = require('gulp-plumber'),
 		autoprefixer = require('gulp-autoprefixer'),
+		size         = require('gulp-size');
 		pixrem       = require('gulp-pixrem');
 
 gulp.task('styles', function(){
 	gulp.src(['app.scss', '_embark.scss','./base/*.scss'])
 	.pipe(plumber())
 	.pipe(sass())
-	// .pipe(autoprefixer('last 2 version'))
+	.pipe(autoprefixer('last 2 version'))
 	.pipe(pixrem())
+	.pipe(size())
 	.pipe(gulp.dest('css/'));
 });
 
